067d3cae8abd04fa32b8b271f107ea5dff2e15b4
Kalaivani Velusamy <kalai.v.vani@gmail.com>
2018-07-03 14:26:56 +0530

    Instagram account authorisation.Added client id, secret, redirect url.

---
+++ example_Instagram/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  example_Instagram
+//
+//  Created by Kalaivani, Velusamy (623-Extern) on 02/07/18.
+//  Copyright © 2018 Kalaivani, Velusamy (623-Extern). All rights reserved.
+//
+
+import Foundation
+struct API{
+    static let INSTAGRAM_AUTHURL = "https://api.instagram.com/oauth/authorize/"
+    static let INSTAGRAM_CLIENT_ID = "af3632c4285a4fff80b6ed056e252ab5"
+    static let INSTAGRAM_CLIENTSERCRET = "01901bcd418c4ad29519347aa13572af"
+    static let INSTAGRAM_REDIRECT_URI = "https://localhost:8888/"
+    static let INSTAGRAM_ACCESS_TOKEN = "51584226.af3632c.9b0b5735803f4f0481990f2bf8b16e22"
+    static let INSTAGRAM_SCOPE = "follower_list+public_content" /* add whatever scope you need https://www.instagram.com/developer/authorization/ */
+}
--- example_Instagram/ViewController.swift
+++ example_Instagram/ViewController.swift
@@ -7,11 +7,17 @@
 //
 
 import UIKit
+import WebKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webKitView: WKWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
+    let authURL=String(format:"%@client_id=%@&redirect_uri=%@&response_type=token&scope=%@&DEBUG=True",arguments: [API.INSTAGRAM_AUTHURL,API.INSTAGRAM_CLIENT_ID,API.INSTAGRAM_REDIRECT_URI, API.INSTAGRAM_SCOPE])
+        let urlRequest = URLRequest.init(url: URL.init(string: authURL)!)
+        webKitView.load(urlRequest)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +26,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension ViewController: UIWebViewDelegate{
+    func webView(_ webView: UIWebView, shouldStartLoadWith request:URLRequest, navigationType: UIWebViewNavigationType) -> Bool{
+        return checkRequestForCallbackURL(request: request)
+    }
+    
+    func checkRequestForCallbackURL(request: URLRequest) -> Bool {
+        let requestURLString = (request.url?.absoluteString)! as String
+        if requestURLString.hasPrefix(API.INSTAGRAM_REDIRECT_URI) {
+            let range: Range<String.Index> = requestURLString.range(of: "#access_token=")!
+            handleAuth(authToken: requestURLString.substring(from: range.upperBound))
+            return false;
+        }
+        return true
+    }
+    func handleAuth(authToken: String) {
+        print("Instagram authentication token ==", authToken)
+    }
 
 }
+
 
